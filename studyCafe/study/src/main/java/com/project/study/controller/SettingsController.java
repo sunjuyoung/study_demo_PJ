48bd25e12f9d44b1fcc39f0f611f9634f112d5e2
sunjuyoung <syseoz@naver.com>
2022-01-28 23:46:54 +0900

    profile edit view

--- studyCafe/study/src/main/java/com/project/study/controller/SettingsController.java
+++ studyCafe/study/src/main/java/com/project/study/controller/SettingsController.java
@@ -2,11 +2,14 @@ package com.project.study.controller;
 
 import com.project.study.auth.CurrentUser;
 import com.project.study.domain.Account;
+import com.project.study.dto.ProfileForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -16,7 +19,16 @@ public class SettingsController {
     @GetMapping("/settings/profile")
     public String profileUpdateForm(@CurrentUser Account account, Model model){
         model.addAttribute(account);
+        model.addAttribute("profile",new ProfileForm(account));
 
-        return "";
+        return "settings/profile";
+    }
+
+    @PostMapping("/settings/profile")
+    public String submitProfile(@CurrentUser Account account, Model model, @ModelAttribute("profile") ProfileForm profileForm){
+        model.addAttribute(account);
+        model.addAttribute("profile",new ProfileForm(account));
+
+        return "settings/profile";
     }
 }
--- studyCafe/study/src/main/java/com/project/study/dto/ProfileForm.java
+++ studyCafe/study/src/main/java/com/project/study/dto/ProfileForm.java
@@ -1,6 +1,9 @@
 package com.project.study.dto;
 
+import com.project.study.domain.Account;
 import lombok.Data;
+
+import javax.persistence.Lob;
 
 @Data
 public class ProfileForm {
@@ -9,4 +12,15 @@ public class ProfileForm {
     private String url;
     private String occupation;
     private String location;
+
+
+    private String profileImage;
+
+    public ProfileForm(Account account){
+        this.bio = account.getBio();
+        this.url = account.getUrl();
+        this.occupation = account.getOccupation();
+        this.location = account.getLocation();
+        this.profileImage = account.getProfileImage();
+    }
 }
