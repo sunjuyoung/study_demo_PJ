69303b4f0716ce94fb05e5ebe89eeee30ff9ca5e
sunjuyoung <syseoz@naver.com>
2022-02-11 14:30:46 +0900

    add html email send

---
+++ studyCafe/study/src/main/java/com/project/study/mail/EmailService.java
@@ -0,0 +1,6 @@
+package com.project.study.mail;
+
+public interface EmailService {
+
+    void sendEmail(EmailMessage emailMessage);
+}
--- studyCafe/study/src/main/java/com/project/study/service/AccountService.java
+++ studyCafe/study/src/main/java/com/project/study/service/AccountService.java
@@ -1,17 +1,22 @@
 package com.project.study.service;
 
 import com.project.study.auth.UserAccount;
+import com.project.study.config.AppProperties;
 import com.project.study.domain.Account;
 import com.project.study.domain.Tag;
 import com.project.study.domain.Zone;
 import com.project.study.dto.SignUpForm;
 import com.project.study.dto.ZoneForm;
+import com.project.study.mail.EmailMessage;
+import com.project.study.mail.EmailService;
 import com.project.study.repository.AccountRepository;
 import com.project.study.repository.ZoneRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,44 +27,54 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @Service
+@Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class AccountService implements UserDetailsService {
 
     private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final EmailService emailService;
     private final PasswordEncoder passwordEncoder;
     private final ModelMapper modelMapper;
     private final ZoneRepository zoneRepository;
+    private final TemplateEngine templateEngine;
+
+    private final AppProperties appProperties;
 
     public Account saveNewAccount(SignUpForm signUpForm) {
         signUpForm.setPassword(passwordEncoder.encode(signUpForm.getPassword()));
         Account account = modelMapper.map(signUpForm, Account.class);
-/*        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(passwordEncoder.encode(signUpForm.getPassword()))
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();*/
         account.generateToken();
         return accountRepository.save(account);
     }
 
     public void sendSignUpConfirmEmail(Account newAccount) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("스터디카페 가입인증");
-        mailMessage.setText("/check-email-token?token="+newAccount.getEmailCheckToken()+"&email="+newAccount.getEmail());
-        javaMailSender.send(mailMessage);
+        Context context = new Context();
+        String link = "/check-email-token?token="+newAccount.getEmailCheckToken()+"&email="+newAccount.getEmail();
+        context.setVariable("link",link);
+        context.setVariable("nickname",newAccount.getNickname());
+        context.setVariable("linkName","이메일 인증하기");
+        context.setVariable("message","스터디 서비스를 사용하려면 링크를 클릭하세요");
+        context.setVariable("host",appProperties.getHost());
+
+        String message = templateEngine.process("mail/simple-link",context);
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(newAccount.getEmail())
+                .subject("스터디 회원 가입 인증")
+                .message(message)
+                .build();
+        emailService.sendEmail(emailMessage);
     }
 
     public void login(Account newAccount) {
@@ -100,12 +115,23 @@ public class AccountService implements UserDetailsService {
     }
 
     public void sendLoginLink(Account account) {
+        Context context = new Context();
+        context.setVariable("link","/check-email-token?token="+account.getEmailCheckToken()+"&email="+account.getEmail());
+        context.setVariable("nickname",account.getNickname());
+        context.setVariable("linkName","이메일 로그인하기");
+        context.setVariable("message","로그인 하려면  링크를 클릭하세요");
+        context.setVariable("host",appProperties.getHost());
+
+        String message = templateEngine.process("mail/simple-link",context);
+
         account.generateToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(account.getEmail());
-        mailMessage.setSubject("로그인 링크");
-        mailMessage.setText("/login-by-email?token="+account.getEmailCheckToken()+"&email="+account.getEmail());
-        javaMailSender.send(mailMessage);
+
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to(account.getEmail())
+                .subject("로그인 링크")
+                .message(message)
+                .build();
+        emailService.sendEmail(emailMessage);
     }
 
 
@@ -113,7 +139,6 @@ public class AccountService implements UserDetailsService {
         //detach 연간관계 toMany 관계는 null 값, lazy로딩 불가능
         Optional<Account> account1 = accountRepository.findById(account.getId());
         account1.ifPresent(a -> a.getTags().add(tag));
-
         //accountRepository.getOne() lazy로딩 이지만 detach라서 불가능
     }
 
