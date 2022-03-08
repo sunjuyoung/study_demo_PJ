6b028ffa9efec9a1269c88dfaded0e532de97ea2
sunjuyoung <syseoz@naver.com>
2022-03-09 00:12:11 +0900

    add event

--- studyCafe/study/src/main/java/com/project/study/controller/EventController.java
+++ studyCafe/study/src/main/java/com/project/study/controller/EventController.java
@@ -10,9 +10,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -29,6 +30,19 @@ public class EventController {
         model.addAttribute(study);
         model.addAttribute(account);
         model.addAttribute(new EventForm());
+
+        return "event/form";
+    }
+    @PostMapping("/new-event")
+    public String submitEvent(@CurrentUser Account account, @PathVariable String path, Model model,
+                              @Valid @ModelAttribute EventForm eventForm , Errors errors){
+        Study study = studyService.getStudyUpdateStatus(account, path);
+        if(errors.hasErrors()){
+            model.addAttribute(study);
+            model.addAttribute(account);
+            return "event/form";
+        }
+        eventService.createEvent(study,eventForm);
 
         return "event/form";
     }
--- studyCafe/study/src/main/java/com/project/study/service/EventService.java
+++ studyCafe/study/src/main/java/com/project/study/service/EventService.java
@@ -1,8 +1,12 @@
 package com.project.study.service;
 
+import com.project.study.domain.Event;
+import com.project.study.domain.Study;
+import com.project.study.dto.EventForm;
 import com.project.study.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,4 +17,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class EventService {
 
     private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
+
+    public void createEvent(Study study, EventForm eventForm) {
+        Event map = modelMapper.map(eventForm, Event.class);
+
+    }
 }
